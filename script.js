5aa4dd0f1e0a4c5a0f5baf1bd71b4724bb98b61d
Leandro-Pinho <leandroessencia@gmail.com>
2023-05-09 11:37:58 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,21 @@
+var leonardo = document.getElementById("leonardo")
+var samantha = document.getElementById("samantha")
+var bruna = document.getElementById("bruna")
+var setaDireita = document.getElementById("seta-direita")
+var setaEsquerda = document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}
+
+
